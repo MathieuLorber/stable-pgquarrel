92f3b2c4fb101979b7fac4c8b3ff9d6dd632b5ce
Mathieu Lorber <mathieu.lorber@gmail.com>
2023-09-22 09:12:41 +0200

    SAVEPOINT

--- prepare-database.sh
+++ prepare-database.sh
@@ -1,15 +1,15 @@
-createdb source
+createdb -h psql -U postgres source
 # TODO permit a dump
 SOURCE_DUMP=/scripts/source.sql
 if test -f "$SOURCE_DUMP"; then
-  psql -d source < $SOURCE_DUMP
+  psql -h psql -U postgres -d source < $SOURCE_DUMP
 fi
 MIGRATION=/scripts/migration.sql
 if test -f "$MIGRATION"; then
-  psql -d source < $MIGRATION
+  psql -h psql -U postgres -d source < $MIGRATION
 fi
-createdb target
+createdb -h psql -U postgres target
 TARGET_DUMP=/scripts/target.sql
 if test -f "$TARGET_DUMP"; then
-  psql -d target < $TARGET_DUMP
+  psql -h psql -U postgres -d target < $TARGET_DUMP
 fi
