89da5ea23606484f621a5c258f8c0d0734595666
Mathieu Lorber <mathieu.lorber@gmail.com>
2023-09-22 11:09:07 +0200

    SAVEPOINT

--- compare.sh
+++ compare.sh
@@ -1,15 +1,19 @@
 createdb -h psql -U postgres --no-password source
 
 # TODO permit a binary dump
-SOURCE_DUMP=/scripts/source.sql
-if test -f "$SOURCE_DUMP"; then
-  psql -h psql -U postgres -d source < $SOURCE_DUMP
-fi
-
 createdb -h psql -U postgres --no-password target
 TARGET_DUMP=/scripts/target.sql
 if test -f "$TARGET_DUMP"; then
   psql -h psql -U postgres -d target < $TARGET_DUMP
+fi
+TARGET_DUMP_COMPRESSED=/scripts/target-compressed.sql
+if test -f "$TARGET_DUMP_COMPRESSED"; then
+  pg_restore -h psql -U postgres -d target --format=c --no-acl --no-owner $TARGET_DUMP_COMPRESSED
+fi
+
+SOURCE_DUMP=/scripts/source.sql
+if test -f "$SOURCE_DUMP"; then
+  psql -h psql -U postgres -d source < $SOURCE_DUMP
 fi
 
 MIGRATION=/scripts/migration.sql
